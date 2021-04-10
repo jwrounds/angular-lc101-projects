0d0756ea71556e85141b5a9c143c36f336696cd0
Jason <jwrounds@usfca.edu>
2021-04-10 15:11:11 -0500

    added css styling to movies, chores, added additional movies, and added a photo

--- lesson1/exercises/src/app/fav-photos/fav-photos.component.ts
+++ lesson1/exercises/src/app/fav-photos/fav-photos.component.ts
@@ -8,7 +8,7 @@ import { Component, OnInit } from '@angular/core';
 export class FavPhotosComponent implements OnInit {
   photosTitle = 'Photo/Image Section Title Here';
   image1 = 'https://www.launchcode.org/assets/icons/target-00c7a0bc7776901e3c5cdc4c36c465f19e41a249bc7267a595f0bfa14ab944b7.png';
-  image2 = '';
+  image2 = 'https://cdn.shopify.com/s/files/1/2868/5752/products/a00bbdddeece6ae3d0fb8eccabb82f67_300x.jpg?v=1572069139';
   image3 = '';
 
   constructor() { }
